d971e00c87527d50bebc953ac32c5278563ae9a2
Marie <marievianca.pagaduan@georgebrown.ca>
2023-03-19 14:29:17 -0400

    added title

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,14 @@ import Logo from '../assets/logo.png';
 
 const Header = () => {
   return (
-    <header className='py-8'>
-      <div className='container mx-auto'>
-        <div className='flex justify-between items-center'>
+    <header className="py-8">
+      <title>Marie Vianca</title>
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
           {/* logo */}
-          <a href='#'>
-            <img src={Logo} alt='' />
+          <a href="#">
+            <img src={Logo} alt="" />
           </a>
-          
-          
         </div>
       </div>
     </header>
